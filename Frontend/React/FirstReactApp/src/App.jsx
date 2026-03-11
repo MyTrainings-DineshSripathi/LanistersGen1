1d5d20d2551618461de480a400272a1120d0cc43
Dinesh Sripathi <dineshsri718@gmail.com>
2026-03-11 11:04:11 +0530

    started cloing the https://themewagon.github.io/cozastore/index.html

--- Frontend/React/FirstReactApp/src/App.jsx
+++ Frontend/React/FirstReactApp/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Slide1 from './assets/slide-01.jpg'
 
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <div className='logo'>
+          Cozy Store
+        </div>
+        <nav>
+          <a className='nav-link' href="">Home</a>
+          <a className='nav-link' href="">Shop</a>
+          <a className='nav-link' href="">Featured Products</a>
+          <a className='nav-link' href="">Blog</a>
+          <a className='nav-link' href="">About</a>
+          <a className='nav-link' href="">Contact</a>
+        </nav>
+        <div className='options'>
+          <button className='nav-link'>search</button>
+          <button className='nav-link'>cart</button>
+          <button className='nav-link'>wishlist</button>
+        </div>
+      </header>
+      <main>
+        <section className="hero-sliding-section">
+          <div className='info-container'>
+            <div className="intro-title">
+              Womens Collection 2026
+            </div>
+            <div className="title">
+              NEW COLLECTION
+            </div>
+            <div className="cta">
+              <a href="">Shop Now</a>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
--- Frontend/React/FirstReactApp/src/main.jsx
+++ Frontend/React/FirstReactApp/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/* 
+  document.getElementById('root') -- we are selecting a element with an id root
+  createRoot -- with this function we are declaring that the selected element as the root to render all the content
+*/
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
